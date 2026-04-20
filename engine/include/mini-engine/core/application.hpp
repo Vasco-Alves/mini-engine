e02eb585d7be7c6e53d1129c46549f320f4010bb
Vasco Alves <vascobernardoalves@gmail.com>
2026-04-20 20:10:34 +0200

    Update application.hpp

--- engine/include/mini-engine/core/application.hpp
+++ engine/include/mini-engine/core/application.hpp
@@ -22,7 +22,8 @@ namespace me {
 
 		// Called when the window changes size
 		virtual void on_resize(int width, int height) {
-			m_active_scene->on_resize(width, height);
+			if (m_active_scene)
+				m_active_scene->on_resize(width, height);
 		}
 
 		// Called every frame. dt = delta time in seconds.
@@ -30,17 +31,20 @@ namespace me {
 
 		// Called every frame after clearing the screen
 		virtual void on_render() {
-			m_active_scene->on_render();
+			if (m_active_scene)
+				m_active_scene->on_render();
 		}
 
 		// Called when the window is closing
 		virtual void on_shutdown() {
-			if (m_active_scene) m_active_scene->on_close();
+			if (m_active_scene)
+				m_active_scene->on_close();
 		}
 
 		// Loads new scene into memory
 		void load_scene(std::shared_ptr<Scene> scene, int current_width, int current_height) {
-			if (m_active_scene) m_active_scene->on_close();
+			if (m_active_scene)
+				m_active_scene->on_close();
 
 			m_active_scene = scene;
 			m_active_scene->on_start(current_width, current_height);
